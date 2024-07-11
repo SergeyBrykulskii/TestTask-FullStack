c82e85effd8ba4d091e8138996ff417d5a6e1331
Sergey Brykulskii <92694237+SergeyBrykulskii@users.noreply.github.com>
2024-07-11 16:35:17 +0300

    added vehicles models (#1)

---
+++ DeveloperTest/VehicleApp/Models/Abstactions/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace VehicleApp.Models.Abstactions;
+
+public abstract class Vehicle
+{
+    public int MaxSpeed { get; set; }
+    public string Brand { get; set; }
+    public string Color { get; set; }
+
+    protected Vehicle(int maxSpeed, string brand, string color)
+    {
+        MaxSpeed = maxSpeed;
+        Brand = brand;
+        Color = color;
+    }
+}
