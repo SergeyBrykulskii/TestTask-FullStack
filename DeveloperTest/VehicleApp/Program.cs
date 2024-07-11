b050545c3c12e1a1bbab5504de8740bd6374ec2e
Sergey Brykulskii <serxio6758sk@gmail.com>
2024-07-11 17:17:44 +0300

    added InstanceService

---
+++ DeveloperTest/VehicleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using VehicleApp.Models.Abstactions;
+using VehicleApp.Services;
+
+namespace VehicleApp;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var vehicles = InstanceService.GetInstances<Vehicle>();
+
+        foreach (var el in vehicles)
+        {
+            Console.WriteLine(el);
+        }
+    }
+}
