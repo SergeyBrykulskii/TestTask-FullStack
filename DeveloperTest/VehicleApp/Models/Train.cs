9762b7fc765c6783817f1c5063a81a6da2a339a7
Sergey Brykulskii <92694237+SergeyBrykulskii@users.noreply.github.com>
2024-07-11 17:23:51 +0300

    Backend-Task-2 (#2)
    * added InstanceService
    
    * added fourth class

---
+++ DeveloperTest/VehicleApp/Models/Train.cs
@@ -0,0 +1,12 @@
+﻿using VehicleApp.Models.Abstactions;
+
+namespace VehicleApp.Models;
+
+public class Train : Vehicle
+{
+    public int NumberOfCars { get; set; }
+    public Train() : base(150, "STRIZH", "grey")
+    {
+        NumberOfCars = 10;
+    }
+}
---
+++ DeveloperTest/VehicleApp/Services/InstanceService.cs
@@ -0,0 +1,15 @@
+﻿namespace VehicleApp.Services;
+
+public static class InstanceService
+{
+    public static IEnumerable<T> GetInstances<T>() where T : class
+    {
+        var types = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(s => s.GetTypes())
+            .Where(currType => currType is { IsClass: true, IsAbstract: false }
+                && currType.IsSubclassOf(typeof(T)))
+            .Select(type => (T)Activator.CreateInstance(type));
+
+        return types;
+    }
+}
